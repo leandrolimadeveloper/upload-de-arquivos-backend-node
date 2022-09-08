76da8dbd29a0cda85bd0cebea37afe54dd0cd280
leandrolimadeveloper <leandroslimasr@gmail.com>
2022-09-07 22:04:20 -0300

    Finished

---
+++ src/index.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const morgan = require('morgan')
+const path = require('path')
+const mongoose = require('mongoose')
+
+/**
+ * Database settings
+ */
+mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Database connected')
+        app.emit('Done')
+    })
+    .catch(err => console.log(err))
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
+
+app.use(require('./routes'))
+
+app.on('Done', () => {
+    const port = 3333
+    app.listen(port, () => {
+        console.log(`Server running at: http://localhost:${port}`)
+    })
+})
